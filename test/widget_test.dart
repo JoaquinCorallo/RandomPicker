564fffb1d59771ad8c47027e129320f6460094bc
JoaquinCorallo <joaquincorallofabiano@gmail.com>
2024-03-06 22:22:18 -0300

    feature/blocStructure

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:random_picker/main.dart';
+import 'package:random_picker/core/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
